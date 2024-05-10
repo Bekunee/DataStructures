5cf626a9ef81e6be3364f16dcc5c0a822610b1b7
Namsraijalbuu Bilguuntuguldur <145345356+Bekunee@users.noreply.github.com>
2024-05-10 14:43:35 +0900

    Update bintree.c

--- code/May10/bintree.c
+++ code/May10/bintree.c
@@ -17,30 +17,51 @@ bintree_t *
 bintree_add_left (bintree_t * parent, bintree_t * child)
 {
 	/*TODO*/
+	parent->left = child ;
 }
 
 bintree_t *
 bintree_add_right (bintree_t * parent, bintree_t * child)
 {
 	/*TODO*/
+	parent->right= child ;
 }
 
 bintree_t *
 bintree_search (bintree_t * t, char key)
 {
 	/*TODO*/
+	if (t->data == key)
+		return t;
+	bintree_t*r ;
+	r = bintree_search(t->left)
+
 }
 
 int
-bintree_is_ancestor (bintree_t * parent, bintree_t * child)
+bintree_is_parent (bintree_t * parent, bintree_t * child)
 {
 	/*TODO*/
+	if(t1->left == t2 || t1->right == t2)
 }
-
+int 
+bintree_is_ancestor (bintree_t * parent, bintree_t * child){
+	if(t1->left == t2 || t1->right == t2)
+	return 1;
+	if (t1->left){
+		if ()
+	}
+}
 void
 bintree_delete (bintree_t * t)
 {
 	/*TODO*/
+	if (t->left)
+		bintree_delete(t->left);
+	if (t->right)
+		bintree_delete(t->right);
+	free(t);
+
 }
 
 
@@ -48,12 +69,28 @@ int
 bintree_height(bintree_t * t) 
 {
 	/*TODO*/
+	if (t->left){
+		int height;
+		height = bintree_height(t->left);
+		if (max_height < height){
+			max_height = height;
+		}
+			
+}
+if (t->right){
+		int height;
+		height = bintree_height(t->right);
+		if (max_height < height){
+			max_height = height;
+		}
+			
 }
 
-
+return max_height + 1 ;
 void
 bintree_print (bintree_t * t)
 {
 	/*TODO*/
+	bintree_print (bintree_t)
 }
 
